cf90ac957ca6494f4f72ad4209d32a60eb26cc83
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-10-03 15:20:34 +0530

    Added: PersonInfo Class to create a contact of a person

--- AddressBookJS.js
+++ AddressBookJS.js
@@ -1,1 +1,43 @@
 console.log("Welcome to Address Book Program");
+class PersonInfo {
+  firstName;
+  lastName;
+  address;
+  city;
+  state;
+  zip;
+  phoneNumber;
+  email;
+  constructor(firstName,lastName,address,city,state,zip,phoneNumber,email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
+  toString() {
+    return (
+      "First Name = " +
+      this.firstName +
+      ", Last Name = " +
+      this.lastName +
+      ",Address = " +
+      this.address +
+      ",City = " +
+      this.city +
+      ",State = " +
+      this.state +
+      ",Zip = " +
+      this.zip +
+      ",Phone Number = " +
+      this.phoneNumber +
+      ",Email = " +
+      this.email
+    );
+  }
+}
+let personInfo = new PersonInfo("Paresh","Praveen","Ganesh Prasad","Mangalore","Karnataka","575014","9731497550","paresh@gmail.com");
+console.log(personInfo.toString());
